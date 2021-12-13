14c5c1355c1632d71125fbc8df28c2d756951a01
Eduardo E Bernal <eduebernal@Eduardos-MacBook-Air.local>
2021-12-13 01:38:30 -0700

    Added day 13

---
+++ aocDay13.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const rawInput = './aocInput13.txt'
+
+    function fileToStr(file){
+        return fs.readFileSync(file,'utf8');
+    }
+
+    function parseData(str){
+        const arr = str.split('\n');
+        const coord = arr.slice(0,arr.indexOf(''))
+        const folds = arr.slice(arr.indexOf('')+1)
+        .map(a=>(a.indexOf('x')>-1)?
+            a.split('').slice(a.indexOf('x')).join('').split('=')
+            :a.split('').slice(a.indexOf('y')).join('').split('=')
+        );
+        return [coord,folds];
+    }
+
+    function fold(arr){
+        let paper = new Map;
+        arr[0].forEach(a=>{     //Add coordinates to map;
+            paper.set(a,a.split(',').map(b=>parseInt(b)));
+        })
+        arr[1].forEach((fold)=>{    //Fold the paper as per instructions
+            let foldLine=parseInt(fold[1]);
+            paper.forEach((point,coord)=>{
+                if(fold[0]=='y'){
+                    if(point[1]>foldLine){
+                        let x = point[0];
+                        let y = foldLine*2-point[1];
+                        paper.set(x+','+y,[x,y]);
+                        paper.delete(coord);
+                    }
+                }
+                else{
+                    if(point[0]>foldLine){
+                        let x = foldLine*2-point[0];
+                        let y = point[1];
+                        paper.set(x+','+y,[x,y]);
+                        paper.delete(coord);
+                    }
+                }
+               
+            })
+        })
+                                //Populate an array with the points from the paper map to visualize
+        let code= [];
+        paper.forEach(value=>{
+            code[value[1]]=[];
+        })
+        paper.forEach(value=>{
+            code[value[1]][value[0]]='█';
+        })
+        for(let i=0;i<code.length;i++){
+            for(let j=0;j<code[0].length;j++){
+                if(code[i][j]!='█'){
+                    code[i][j]='░';
+                }
+            }
+        }
+        return code.map(a=>a.join(''));
+    }
+
+    let puzzleInput = parseData(fileToStr(rawInput));
+    
+    console.log('The code is:')
+    console.log(fold(puzzleInput));
